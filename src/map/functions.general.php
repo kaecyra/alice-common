a8092b8a544e1afbecd7d946c8d0116c9e4dffcb
Timothy Gunter <tim@vanillaforums.com>
2016-06-05 21:29:35 -0400

    Add ordinal()

--- src/map/functions.general.php
+++ src/map/functions.general.php
@@ -357,6 +357,21 @@ function formatStringCallback($match, $setArgs = false) {
     return $result;
 }
 
+/**
+ * Get string ordinal for number
+ * 
+ * @param integer $number
+ * @return string
+ */
+function ordinal($number) {
+    $ends = array('th','st','nd','rd','th','th','th','th','th','th');
+    if ((($number % 100) >= 11) && (($number%100) <= 13)) {
+        return $number. 'th';
+    } else {
+        return $number. $ends[$number % 10];
+    }
+}
+
 /** Checks whether or not string $haystack begins with string $needle.
  *
  * @param string $haystack The main string to check.
